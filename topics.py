f6a8ed945544ecb3442f880c70727635990d9c53
mrcod3r.ir <mrcod3r.ir@gmail.com>
2024-02-13 23:26:52 +0330

    init

---
+++ topics.py
@@ -0,0 +1,64 @@
+'''
+Topics :
+
+- intro
+- syntax
+- comments
+- variables
+- data types
+- number intro
+- string intro
+- boolean intro
+- collections intro
+- list intro
+- dictionary intro
+- operators
+- if-else intro
+- while loops intro
+- break / continue
+
+- for loops intro
+- functions intro
+- number methods 1
+- string methods 1
+- boolean methods 1
+- list methods 1
+- dictionary methods 1
+- if-elif-else
+
+- while loops : else/pass
+- dictionary methods 2 : nested dictionary 
+- for loops 2 : nested/range/pass/else
+- functions 2
+- exceptions
+- try/except
+- tuples intro
+- sets intro
+
+- number methods 2
+- string methods 2
+- boolean methods 2
+- list methods 2
+- dictionary methods 2
+- files
+- keywords
+- lambda
+
+- modules
+- os
+- csv
+'''
+
+
+'''
+
+intro :
+  - intro
+  - used for ?
+  - what can python do?
+  - why python ?
+  
+syntax :
+
+
+'''
